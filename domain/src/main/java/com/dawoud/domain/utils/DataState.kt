c365882309540073d58ba0c0e8cf1737953c4ed7
Ashraf <ashrafdawoud1111@gmail.com>
2022-07-31 00:05:01 +0200

    Remove unwanteed folder

--- domain/src/main/java/com/dawoud/domain/utils/DataState.kt
+++
@@ -1,8 +0,0 @@
-package com.dawoud.domain.utils
-
-sealed class DataState<out R> {
-    data class Success<out T>(val data: T) : DataState<T>()
-    data class Error<out T>(val exception: Exception , val data : T?) : DataState<T>()
-    object Loading : DataState<Nothing>()
-}
-
